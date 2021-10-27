a852f58652c9bad9fdd6b30ce631dbc2ce7fd89a
Keshav <keshavkumar14092001@gmail.com>
2021-10-27 11:01:21 +0530

    Still in the initial Process

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,4 +2,4 @@
 
 let mix = require('laravel-mix');
 
-mix.js('src/app.js', 'dist').setPublicPath('dist');
+mix.js('resources/js/app.js', 'public/js/app.js').sass('resources/scss/app.scss', 'public/css/app.css');
