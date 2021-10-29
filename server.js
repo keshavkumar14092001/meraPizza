1e53648ef8f1e24cabdc6d2b8728721e65252b1a
Keshav <keshavkumar14092001@gmail.com>
2021-10-29 20:49:19 +0530

    Completed Cart,Login,Register and Menu Page

--- server.js
+++ server.js
@@ -8,18 +8,38 @@ const PORT = process.env.PORT || 5000;
 // Declaring Public/STatic Folder:
 app.use(express.static('public'));
 
+// Setting Template Engine:
+app.use(expressLayout);
+app.set('views', path.join(__dirname, '/resources/views'));
+app.set('view engine', 'ejs');
+
 // Path
 app.get('/', (req, res) => {
     res.render('home');
     res.end();
 })
 
-// Setting Template Engine:
-app.use(expressLayout);
-app.set('views', path.join(__dirname, '/resources/views'));
-app.set('view engine', 'ejs');
+app.get('/cart', (req, res) => {
+    res.render('customers/cart');
+    res.end();
+})
 
+app.get('/menu', (req, res) => {
+    res.render('menu');
+    res.end();
+})
 
+app.get('/register', (req, res) => {
+    res.render('auth/register');
+    res.end();
+})
+
+app.get('/login', (req, res) => {
+    res.render('auth/login');
+    res.end();
+})
+
+// Listening to PORT:
 app.listen(PORT, () => {
     console.log(`The Server is listening at PORT ${PORT}`);
 })
